15562c82bfb54d4ed55359944a098fe8cdd48c9b
JuGid <gidjulien@gmail.com>
2015-12-29 14:44:25 +0100

    merge de model blank et model url
    Ajout des pages model_note_edit.php et model_note_view.php (vides)

--- src/model_index.php
+++ src/model_index.php
@@ -1,4 +1,21 @@
 <?php
+//model for blank URL at index milinks.info/ 
+function generateUrl(){
+	global $pdo; // get PDO connection
+	// generate a <10 characters (numerical and alphabetical) string, not present in the DB ex: ajf63
+	$pool = 'abcdefghijklmnopqrstuvwxyz0123456789';
+	$url = '';
+	for ($i = 0; $i < 7; $i++) {
+	    $url .= $pool[rand(0, strlen($pool) - 1)];
+	}
+	$validation = $pdo->query("SELECT `id` FROM `note` WHERE `id` = '$url'");
+	if($validation->rowCount() > 0) {
+		generateUrl();
+    }
+    else {
+        return $url;	
+    }
+}
 //model for milinks.info/{url}
 function checkUrl($url){
 	global $pdo; // get PDO connection
---
+++ src/model_index_url_update.php
@@ -0,0 +1,5 @@
+<?php
+//model for blank URL at index milinks.info/ 
+function updateNote($url){
+	global $pdo; // get PDO connection
+	$upadting = $pdo->query("UPDATE )
